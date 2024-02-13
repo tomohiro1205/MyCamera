bbf3f3dfa5d0398da505e8325781f1b22d780177
tomohiro1205 <tomohiro_1205@yahoo.co.jp>
2024-02-13 14:29:08 +0900

    カメラアプリ前半終わり

--- MyCamera/ContentView.swift
+++ MyCamera/ContentView.swift
@@ -6,10 +6,30 @@
 //
 
 import SwiftUI
+import PhotosUI
 
 struct ContentView: View {
+    // 撮影した写真を保持する状態変数
+    @State var captureImage: UIImage? = nil
+    // 撮影画面(sheet)の開閉状態を管理
+    @State var inShowSheet = false
+    // フォトライブラリーで選択した写真を管理
+    @State var photoPickerSelectedImage: PhotosPickerItem? = nil
+
     var body: some View {
         VStack {
+            // スペース追加
+            Spacer()
+            // 撮影した写真があるとき
+            if let captureImage {
+                // 撮影写真を表示
+                Image(uiImage: captureImage)
+                //リサイズ
+                    .resizable()
+                // アスペクト比(縦横比)を維持して画面に収める
+                    .scaledToFit()
+            }
+
             // スペース追加
             Spacer()
             // 「カメラを起動する」ボタン
@@ -18,7 +38,9 @@ struct ContentView: View {
                 // カメラが利用可能かチェック
                 if UIImagePickerController.isSourceTypeAvailable(.camera) {
                     print("カメラは利用できます")
-                }　else {
+                    // カメラが使えるなら、isShowSheetをtrue
+                    inShowSheet.toggle()
+                } else {
                     print("カメラは利用できません")
                 }
             } label: {
@@ -35,6 +57,70 @@ struct ContentView: View {
                 // 文字色を白色に指定
                     .foregroundStyle(Color.white)
             } // 「カメラを起動する」ボタンここまで
+            // 上下左右に余白を追加
+            .padding()
+            // sheetを表示
+            // isPresentedで指定した状態変数がtrueのとき実行
+            .sheet(isPresented: $inShowSheet) {
+                // UIImagePickerController(写真撮影)を表示
+                ImagePickerView(isShowSheet: $inShowSheet, captureImage: $captureImage)
+            } //「カメラを起動する」ボタンのsheetここまで
+
+            // フォトライブラリーから選択する
+            PhotosPicker(selection: $photoPickerSelectedImage, matching: .images, preferredItemEncoding: .automatic, photoLibrary: .shared()) {
+                // テキスト表示
+                Text("フォトライブラリーから選択する")
+                // 横幅いっぱい
+                    .frame(maxWidth: /*@START_MENU_TOKEN@*/.infinity/*@END_MENU_TOKEN@*/)
+                // 高さ50ポイント指定
+                    .frame(height: 50)
+                // 背景を青色に指定
+                    .background(Color.blue)
+                // 文字色を白色に指定
+                    .foregroundStyle(Color.white)
+                // 上下左右に余白を追加
+                    .padding()
+            } // PhotosPickerここまで
+            // 選択した写真情報をもとに写真を取り出す
+            .onChange(of: photoPickerSelectedImage, initial: true, { oldValue, newValue in
+                // 選択した写真があるとき
+                if let newValue {
+                    // Data型で写真を取り出す
+                    newValue.loadTransferable(type: Data.self) { result in
+                        switch result {
+                        case .success(let data):
+                            // 写真があるとき
+                            if let data {
+                                // 写真をcaptureImageに保存
+                                captureImage = UIImage(data: data)
+                            }
+                        case .failure:
+                            return
+                        }
+                    }
+                }
+            }) //.onChange ここまで
+
+            // captureImageをアンラップする
+            if let captureImage {
+                // captureImageから共有する画像を生成する
+                let shareImage = Image(uiImage: captureImage)
+                //共有シート
+                ShareLink(item: shareImage, subject: nil, message: nil, preview: SharePreview("Photo", image: shareImage)) {
+                    // テキスト表示
+                    Text("SNSに投稿する")
+                    // 横幅いっぱい
+                        .frame(maxWidth: /*@START_MENU_TOKEN@*/.infinity/*@END_MENU_TOKEN@*/)
+                    // 高さ50ポイント指定
+                        .frame(height: 50)
+                    // 背景を青色に指定
+                        .background(Color.blue)
+                    // 文字色を白色に指定
+                        .foregroundStyle(Color.white)
+                    // 上下左右に余白を追加
+                        .padding()
+                } // ShareLinkここまで
+            } // アンラップ ここまで
         } //VStackここまで
     } //bodyここまで
 } // contentView ここまで
